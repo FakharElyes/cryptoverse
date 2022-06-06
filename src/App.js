fa5589b3adf5161d36a7908111e04b0d99e5719b
FakharElyes <elyes.fakhar@esprit.tn>
2022-06-06 12:57:19 +0100

    static data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Switch, Route, Link } from 'react-router-dom';
+import { Routes , Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
-import { Navbar } from './components';
+import { Exchanges, Homepage, News, Cryptocurrencies, CryptoDetails, Navbar } from './components';
+import './App.css';
 
 const  App = () => {
   return (
@@ -11,12 +12,31 @@ const  App = () => {
             <Navbar />
         </div>
         <div className="main">
-
+          <Layout>
+            <div className="routes">
+              <Routes >
+                <Route path='/' element={<Homepage/>} />
+                <Route path='/exchanges' element={<Exchanges/>} />
+                <Route path='/cryptocurrencies' element={<Cryptocurrencies/>} />
+                <Route path='/crypto/:coinId' element={<CryptoDetails/>} />
+                <Route path='/news' element={<News/>} />
+              </Routes >
+            </div>
+          </Layout>
+          <div className="footer">
+            <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
+              <Link to="/">
+                Cryptoverse Inc.
+              </Link> <br />
+              All Rights Reserved.
+            </Typography.Title>
+            <Space>
+              <Link to="/">Home</Link>
+              <Link to="/exchanges">Exchanges</Link>
+              <Link to="/news">News</Link>
+            </Space>
         </div>
-
-        <div className="footer">
-            
-        </div>
+      </div>
     </div>
 
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 
+import 'antd/dist/antd.css';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
     <Router>
         <App />
-    </Router>,
-    document.getElementById('root'),
+    </Router>
+  </React.StrictMode>
 );
